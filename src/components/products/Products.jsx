a3f2811eb2a749b80e66b6f471616d36f9c9fa54
Linn Johansen <linn.es.johansen@gmail.com>
2025-02-05 16:14:39 -0800

    Minor CSS fixes Product page

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -25,26 +25,28 @@ function Products() {
   }, [categoryName, dispatch]);
 
   return (
-    <div className="flex w-full flex-col gap-3">
-      {/* Image */}
-      <div className="mx-auto mt-2 flex h-[7vh] min-h-fit w-full max-w-[1320px] items-center justify-center rounded-lg bg-gradient-to-r from-aura via-mint to-zest py-2 text-pearl">
-        <h2
-          className="text-shad w-full pl-16 font-bebas text-6xl uppercase tracking-widest"
-          style={{ textShadow: "2px 2px 4px rgba(0, 0, 0, 0.3)" }}
-        >
-          {categoryName}
-        </h2>
-      </div>
+    <div className="flex w-full justify-center">
+      <div className="flex w-full max-w-[1284px] flex-col gap-3 px-2">
+        {/* Banner */}
+        <div className="mt-2 h-[7vh] min-h-fit w-full rounded-lg bg-gradient-to-r from-aura via-mint to-zest py-2 text-pearl">
+          <h2
+            className="w-full pl-16 font-bebas text-6xl uppercase tracking-widest"
+            style={{ textShadow: "2px 2px 4px rgba(0, 0, 0, 0.3)" }}
+          >
+            {categoryName}
+          </h2>
+        </div>
 
-      <div className="mx-auto flex w-full min-w-fit max-w-[1284px] flex-col space-y-3 px-2">
-        {/* Filter & Sort menu */}
-        <RefineMenu />
+        <div className="mx-auto flex w-full min-w-fit max-w-[1284px] flex-col space-y-3">
+          {/* Filter & Sort menu */}
+          <RefineMenu />
 
-        {/* Product cards */}
-        <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
-          {products.map((product) => (
-            <ProductCard key={product._id} product={product} />
-          ))}
+          {/* Product cards */}
+          <div className="grid grid-cols-2 gap-4 px-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
+            {products.map((product) => (
+              <ProductCard key={product._id} product={product} />
+            ))}
+          </div>
         </div>
       </div>
     </div>
